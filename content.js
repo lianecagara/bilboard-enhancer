9eb80a1c198835063d511b4bb7d154bc57d5c3a7
lianecagara <nealiana.kaye.cagara@gmail.com>
2024-11-16 23:01:17 +0800

    select

--- content.js
+++ content.js
@@ -1,5 +1,16 @@
 window.onload = () => {
   console.log("Bilboard Enhancer Started!");
+  const style = document.createElement("style");
+  style.textContent = `
+  * {
+    user-select: text !important;
+    -webkit-user-select: text !important;
+    -ms-user-select: text !important;
+    -moz-user-select: text !important;
+    pointer-events: auto !important;
+  }
+`;
+  document.head.appendChild(style);
 
   if (/*window.location.pathname === "/user_dashboard" */ true) {
     const mainPgCont = document.querySelector(
